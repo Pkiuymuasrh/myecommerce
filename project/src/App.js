0a14036e7396aecd262a4e0afd039847c3655c5e
Pkiuymuasrh <168890628+Pkiuymuasrh@users.noreply.github.com>
2024-07-11 23:51:08 +0530

    checkout

--- project/src/App.js
+++ project/src/App.js
@@ -36,13 +36,4 @@ const router = createBrowserRouter([
   }
 ]);
 
-function App() {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
 
-    </div>
-  );
-}
-
-export default App;
